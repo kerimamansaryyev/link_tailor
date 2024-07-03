3fc44c8aecb32ef15a76e41285c2dc195136855c
kerimamansaryyev <keamansaryyev@gmail.com>
2024-07-03 15:12:45 -0500

    Set up the dependencies

--- lib/link_tailor.dart
+++ lib/link_tailor.dart
@@ -1,13 +1,3 @@
 import 'package:gazelle_core/gazelle_core.dart';
-import 'routes/hello_gazelle_route/hello_gazelle_route.dart';
 
-Future<void> runApp(List<String> args) async {
-  final app = GazelleApp(
-    routes: [
-      helloGazelleRoute,
-    ],
-  );
-
-  await app.start();
-  print("Gazelle listening at ${app.serverAddress}");
-}
+Future<void> runApp(List<String> args) async {}
